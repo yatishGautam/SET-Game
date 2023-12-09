3c9115df27ac8562ac5eeb36085932fd3e3b0f8f
yatish <ydipankar@gmail.com>
2023-12-09 16:43:16 -0500

    changes

---
+++ SetGame/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  SetGame
+//
+//  Created by Yatish Gautam on 2023-12-09.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Yatish Gautam on 2023-12-09.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
